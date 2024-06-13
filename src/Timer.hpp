b9a0d38308cde84ef5d34af6bdaee1c3a7526a8f
Sebastian Zientek <sebastian.zientek1@gmail.com>
2024-06-13 17:03:01 +0200

    Added timer class and name for MqttDevice

---
+++ src/Timer.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <Arduino.h>
+
+#include <functional>
+
+class Timer
+{
+    using FunType = std::function<void()>;
+
+public:
+    void setCallback(const FunType &fun)
+    {
+        m_function = fun;
+    }
+
+    void start(std::size_t timeoutMillis, bool repeat = false)
+    {
+        m_period = timeoutMillis;
+        m_startTime = millis();
+
+        m_stopped = false;
+        m_repeat = repeat;
+    }
+
+    void stop()
+    {
+        m_stopped = true;
+    }
+    
+    void update()
+    {
+        if (!m_stopped && m_period + m_startTime <= millis())
+        {
+            m_function();
+
+            if (m_repeat)
+            {
+                m_startTime = millis();
+            }
+            else
+            {
+                m_stopped = true;
+            }
+        }
+    }
+
+private:
+    FunType m_function = [] {};
+    bool m_stopped = false;
+    bool m_repeat = false;
+    std::size_t m_period = 0;
+    std::size_t m_startTime = 0;
+};
